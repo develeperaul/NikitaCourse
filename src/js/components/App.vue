1216f0ccd2bb30116ad941815c8018977f6839b3
Ralfik <developeraul@gmail.com>
2022-01-14 13:57:53 +0500

    верстка блоков

---
+++ src/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="example">sdlkgfjdlkfgjdlkfgjdlkfjgldkjfsg;ljkfglksdjfg;j</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
